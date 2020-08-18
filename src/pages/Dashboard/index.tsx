c3d3991814f1f9a121953d7f8018f89c45e3c3cb
pedroigor66 <picoliveira92@gmail.com>
2020-08-18 14:36:45 -0300

    User is now redirected to dashboard if authenticated. Otherwise redirected to login

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+// This route can only be acessible if the user is logged in
+// check the routes index file to see how this is done
+export default Dashboard;
