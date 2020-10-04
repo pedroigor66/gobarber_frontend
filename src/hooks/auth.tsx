1a51a5de34a281183a7f4677079f990cbccf3356
pedroigor66 <picoliveira92@gmail.com>
2020-10-04 17:39:29 -0300

    now weekdays that have passed are faded out. Token is passed more efficiently in auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,8 +31,10 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
+    // the above code is executed when a user refreshes the webpage
 
     return {} as AuthState;
   });
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
